63f978a6998c5cff1949b918aace3d6530256655
DevonL <DevonL@Mvpsi.com>
2014-08-25 12:52:41 -0400

    Update of Get-JAMSResources
    Updated variables

--- Get-JAMSResources.ps1
+++ Get-JAMSResources.ps1
@@ -1,5 +1,5 @@
 function Get-JAMSResources {
     pushd
-    Invoke-Sqlcmd -ServerInstance <jams_db_server> -Database <jams_db_name> -Query "SELECT M.resource_name, M.qty_available, IsNull((select SUM(U.qty_in_use) from dbo.ResourceInUse as U where U.resource_id = M.resource_id), 0) as InUse FROM dbo.ResourceM as M"
+    Invoke-Sqlcmd -ServerInstance <<jams_db_server>> -Database <<jams_db_name>> -Query "SELECT M.resource_name, M.qty_available, IsNull((select SUM(U.qty_in_use) from dbo.ResourceInUse as U where U.resource_id = M.resource_id), 0) as InUse FROM dbo.ResourceM as M"
     popd
 }
